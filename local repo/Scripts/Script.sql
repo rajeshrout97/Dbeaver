00a3d6335482dce2abeb84da0c4df3f186f3a01f
Rajesh Rout <routr5953@gmail.com>
2022-01-11 11:54:26 +0530

    Sql script is saved

---
+++ local repo/Scripts/Script.sql
@@ -0,0 +1,3 @@
+create or replace table miniproject.PUBLIC.Amazon_test (show_id varchar(20) , type varchar(20),
+title varchar(200),director varchar(20),cast varchar(300),country varchar(20),
+release_year number(10,0),rating number(10,3),listed_in varchar(200));
